c0f1872e74b87d277b7eb72ad6fccdaff0da5033
nbmx <nbmx@92290d91-35b6-4caa-bc0e-b1f30167e394>
2010-05-06 20:44:24 +0000

    Alpha channel id dds decompress fix

--- branches/dp/Texture.cpp
+++ branches/dp/Texture.cpp
@@ -486,7 +486,7 @@ TTexturesManager::AlphaValue TTexturesManager::LoadDDS(std::string fileName)
 
         GLuint size = ((data.width + 3) / 4) * ((data.height+3)/4) * data.blockSize;
 
-        if((Global::bDecompressDDS)&&(i==(data.numMipMaps-1)))
+        if((Global::bDecompressDDS)&&(i==1))  //should be i==0 but then problem with "glBindTexture()"
         {
             GLuint decomp_size = data.width * data.height * 4;
             GLubyte* output = new GLubyte[decomp_size];
