a8e757b55dc2f970030bcd554936fd9a0a88267e
youBy <youBy@92290d91-35b6-4caa-bc0e-b1f30167e394>
2012-12-22 17:00:02 +0000

    Napisany od nowa rozrząd (funkcja AutoRelayCheck)

---
+++ branches/dp_ARC/sky.h
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+
+#ifndef skyH
+#define skyH
+
+#include "MdlMngr.h"
+
+class TSky
+{
+private:
+  TModel3d *mdCloud;
+public:
+  __fastcall TSky();
+  __fastcall ~TSky();
+  void __fastcall Init();
+  void __fastcall Render();
+};
+
+//---------------------------------------------------------------------------
+#endif
