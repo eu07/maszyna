32d4ef00171c1dd16c1971a0a68a246eb2f1d113
Ra <Ra@92290d91-35b6-4caa-bc0e-b1f30167e394>
2013-01-27 22:34:33 +0000

    Do opracowania: wybor karty graficznej.

--- branches/dp/World.cpp
+++ branches/dp/World.cpp
@@ -103,6 +103,61 @@ GLvoid __fastcall TWorld::glPrint(const char *txt) //custom GL "Print" routine
  }
 }
 
+/* Ra: do opracowania: wybor karty graficznej ~Intel gdy s¹ dwie...
+BOOL GetDisplayMonitorInfo(int nDeviceIndex, LPSTR lpszMonitorInfo)
+{
+    FARPROC EnumDisplayDevices;
+    HINSTANCE  hInstUser32;
+    DISPLAY_DEVICE DispDev;
+    char szSaveDeviceName[33];  // 32 + 1 for the null-terminator
+    BOOL bRet = TRUE;
+        HRESULT hr;
+
+    hInstUser32 = LoadLibrary("c:\\windows\User32.DLL");
+    if (!hInstUser32) return FALSE;
+
+    // Get the address of the EnumDisplayDevices function
+    EnumDisplayDevices = (FARPROC)GetProcAddress(hInstUser32,"EnumDisplayDevicesA");
+    if (!EnumDisplayDevices) {
+        FreeLibrary(hInstUser32);
+        return FALSE;
+    }
+
+    ZeroMemory(&DispDev, sizeof(DispDev));
+    DispDev.cb = sizeof(DispDev);
+
+    // After the first call to EnumDisplayDevices,
+    // DispDev.DeviceString is the adapter name
+    if (EnumDisplayDevices(NULL, nDeviceIndex, &DispDev, 0))
+        {
+                hr = StringCchCopy(szSaveDeviceName, 33, DispDev.DeviceName);
+                if (FAILED(hr))
+                {
+                // TODO: write error handler
+                }
+
+        // After second call, DispDev.DeviceString is the
+        // monitor name for that device
+        EnumDisplayDevices(szSaveDeviceName, 0, &DispDev, 0);
+
+                // In the following, lpszMonitorInfo must be 128 + 1 for
+                // the null-terminator.
+                hr = StringCchCopy(lpszMonitorInfo, 129, DispDev.DeviceString);
+                if (FAILED(hr))
+                {
+                // TODO: write error handler
+                }
+
+    } else    {
+        bRet = FALSE;
+    }
+
+    FreeLibrary(hInstUser32);
+
+    return bRet;
+}
+*/
+
 bool __fastcall TWorld::Init(HWND NhWnd,HDC hDC)
 {
  double time=(double)Now();
