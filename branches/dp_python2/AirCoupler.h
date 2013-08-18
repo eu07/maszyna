856195d077a82d437ec424fe7dd0cd0e5e72b1f1
mamut <mamut@92290d91-35b6-4caa-bc0e-b1f30167e394>
2013-08-18 18:58:38 +0000

    New branch for Python Integration v2 (complete rewrite for new integration concept based on SPKS)

---
+++ branches/dp_python2/AirCoupler.h
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+
+#ifndef AirCouplerH
+#define AirCouplerH
+
+#include "Model3d.h"
+#include "QueryParserComp.hpp"
+
+class TAirCoupler
+{
+private:
+//    TButtonType eType;
+ TSubModel *pModelOn,*pModelOff,*pModelxOn;
+ bool bOn;
+ bool bxOn;
+ void __fastcall Update();
+public:
+  __fastcall TAirCoupler();
+  __fastcall ~TAirCoupler();
+  void __fastcall Clear();
+  inline void TurnOn()  { bOn=true;  bxOn=false; Update(); };
+  inline void TurnOff() { bOn=false; bxOn=false; Update(); };
+  inline void TurnxOn() { bOn=false; bxOn=true;  Update(); };
+//  inline bool Active() { if ((pModelOn)||(pModelOff)) return true; return false;};
+  int __fastcall GetStatus();
+  void __fastcall Init(AnsiString asName, TModel3d *pModel);
+  void __fastcall Load(TQueryParserComp *Parser, TModel3d *pModel);
+//  bool __fastcall Render();
+};
+
+//---------------------------------------------------------------------------
+#endif
---
+++ branches/dp_python2/Traction.h
@@ -0,0 +1,61 @@
+//---------------------------------------------------------------------------
+
+#ifndef TractionH
+#define TractionH
+
+#include "opengl/glew.h"
+#include "dumb3d.h"
+#include "VBO.h"
+
+using namespace Math3D;
+
+class TTraction 
+{
+private:
+ vector3 vUp,vFront,vLeft;
+ matrix4x4 mMatrix;
+ //matryca do wyliczania pozycji drutu w zale¿noœci od [X,Y,k¹t] w scenerii:
+ // - x: odleg³oœæ w bok (czy odbierak siê nie zsun¹³)
+ // - y: przyjmuje wartoœæ <0,1>, jeœli pod drutem (wzd³u¿)
+ // - z: wysokoœæ bezwzglêdna drutu w danym miejsu
+public: //na razie
+ TTraction *pPrev,*pNext; //³¹czenie drutów w sieæ
+ int iPrev,iNext; //do którego koñca siê ³¹czy
+public:
+    GLuint uiDisplayList;
+    vector3 pPoint1,pPoint2,pPoint3,pPoint4;
+    double fHeightDifference;//,fMiddleHeight;
+  //  int iCategory,iMaterial,iDamageFlag;
+//    float fU,fR,fMaxI,fWireThickness;
+    int iNumSections;
+    int iLines; //ilosc linii dla VBO
+    float NominalVoltage;
+    float MaxCurrent;
+    float Resistivity;
+    DWORD Material;   //1: Cu, 2: Al
+    float WireThickness;
+    DWORD DamageFlag; //1: zasniedziale, 128: zerwana
+    int Wires;
+    float WireOffset;
+    AnsiString asPowerSupplyName; //McZapkie: nazwa podstacji trakcyjnej
+//    bool bVisible;
+//    DWORD dwFlags;
+
+    void __fastcall Optimize();
+
+    TTraction();
+    ~TTraction();
+
+//    virtual void __fastcall InitCenter(vector3 Angles, vector3 pOrigin);
+//    virtual bool __fastcall Hit(double x, double z, vector3 &hitPoint, vector3 &hitDirection) { return TNode::Hit(x,z,hitPoint,hitDirection); };
+  //  virtual bool __fastcall Move(double dx, double dy, double dz) { return false; };
+//    virtual void __fastcall SelectedRender();
+ void __fastcall RenderDL(float mgn);
+ int __fastcall RaArrayPrepare();
+ void __fastcall RaArrayFill(CVertNormTex *Vert);
+ void __fastcall RenderVBO(float mgn,int iPtr);
+ int __fastcall TestPoint(vector3 *Point);
+ void __fastcall Connect(int my,TTraction *with,int to);
+};
+//---------------------------------------------------------------------------
+#endif
