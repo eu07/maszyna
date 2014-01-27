ddc303d25186424719c3c863f539deb8c5fbc6e9
Ra <Ra@92290d91-35b6-4caa-bc0e-b1f30167e394>
2014-01-27 10:02:06 +0000

    Opcja logowania błędnie zasilanych przęseł (na razie wyłączona).

--- RealSound.cpp
+++ RealSound.cpp
@@ -47,7 +47,7 @@ void __fastcall TRealSound::Init(char *SoundName, double DistanceAttenuation, do
   if (fFrequency!=22050.0)
   {//dla modulowanych nie mo¿e byæ zmiany mno¿nika, bo czêstotliwoœæ w nag³ówku by³¹ ignorowana, a mog³a byæ inna ni¿ 22050
    fFrequency=22050.0;
-   ErrorLog("Bad sound: "+AnsiString(SoundName)+" as modulated, should have 22.05kHz in header");
+   ErrorLog("Bad sound: "+AnsiString(SoundName)+", as modulated, should have 22.05kHz in header");
   }
  if (pSound)
  {
