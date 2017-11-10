eu07/maszyna

commit 3fe968544add18d4d9b466e7a214476a1eb59f2d

TAirCoupler - partial refactor
 - rename private members
 - refactor TAirCoupler::GetStatus()
 - add some English comments

 - add 'doc/' in .gitingore

TODO:
 - rename arguments when refactor other files (as TModel3d, cParser).

AirCoupler.cpp
@@ -19,60 +19,73 @@ TAirCoupler::~TAirCoupler()
 {
 }
 
+// zwraca 1, jeśli istnieje model prosty, 2 gdy skośny
+/**
+ * Returns 0 when straight model exists,
+ * 2 when oblique model exists, and 0 when neither of them exist.
+ */
 int TAirCoupler::GetStatus()
-{ // zwraca 1, jeśli istnieje model prosty, 2 gdy skośny
-    int x = 0;
-    if (pModelOn)
-        x = 1;
-    if (pModelxOn)
-        x = 2;
-    return x;
+{
+    if (ModelOn) return 1;
+    if (ModelxOn) return 2;
+    return 0;
 }
 
+// zerowanie wskaźników
+/**
+ * Reset pointers.
+ */
 void TAirCoupler::Clear()
-{ // zerowanie wskaźników
-    pModelOn = NULL;
-    pModelOff = NULL;
-    pModelxOn = NULL;
-    bOn = false;
-    bxOn = false;
+{
+    ModelOn = NULL;
+    ModelOff = NULL;
+    ModelxOn = NULL;
+    On = false;
+    xOn = false;
 }
-
-void TAirCoupler::Init(std::string const &asName, TModel3d *pModel)
-{ // wyszukanie submodeli
-    if (!pModel)
+// wyszukanie submodeli
+/**
+ * Looks for submodels.
+ */
+void TAirCoupler::Init(std::string const &asName, TModel3d *Model)
+{
+    if (!Model)
         return; // nie ma w czym szukać
-    pModelOn = pModel->GetFromName( asName + "_on" ); // połączony na wprost
-    pModelOff = pModel->GetFromName( asName + "_off" ); // odwieszony
-    pModelxOn = pModel->GetFromName( asName + "_xon" ); // połączony na skos
+    // polaczony na wprost
+    /** Straight connect. */
+    ModelOn = Model->GetFromName(asName + "_on");
+    // odwieszony
+    /** Not connected. Hung up. */
+    ModelOff = Model->GetFromName(asName + "_off");
+    // polaczony na skos
+    /** Oblique connect. */
+    ModelxOn = Model->GetFromName(asName + "_xon");
 }
 
-void TAirCoupler::Load(cParser *Parser, TModel3d *pModel)
+void TAirCoupler::Load(cParser *Parser, TModel3d *Model)
 {
 	std::string name = Parser->getToken<std::string>();
-	if( pModel ) {
-
-		Init( name, pModel );
+    if(Model)
+    {
+		Init(name, Model);
 	}
     else
     {
-        pModelOn = NULL;
-        pModelxOn = NULL;
-        pModelOff = NULL;
+        ModelOn = NULL;
+        ModelxOn = NULL;
+        ModelOff = NULL;
     }
 }
 
 void TAirCoupler::Update()
 {
-    //  if ((pModelOn!=NULL) && (pModelOn!=NULL))
+    //  if ((pModelOn!=NULL) && (pModelOn!=NULL)) // legacy bullshitt alert
     {
-        if (pModelOn)
-            pModelOn->iVisible = bOn;
-        if (pModelOff)
-            pModelOff->iVisible = !(bOn || bxOn);
-        if (pModelxOn)
-            pModelxOn->iVisible = bxOn;
+        if (ModelOn)
+            ModelOn->iVisible = On;
+        if (ModelOff)
+            ModelOff->iVisible = !(On || xOn);
+        if (ModelxOn)
+            ModelxOn->iVisible = xOn;
     }
 }
-
-//---------------------------------------------------------------------------

.gitignore
@@ -74,4 +74,6 @@ builds/build_*
 builds/*.zip
 builds/deps_*
 
+doc/
+
 CMakeLists.txt.user

AirCoupler.h
@@ -14,40 +14,38 @@ http://mozilla.org/MPL/2.0/.
 
 class TAirCoupler
 {
-  private:
-    //    TButtonType eType;
-    TSubModel *pModelOn, *pModelOff, *pModelxOn;
-    bool bOn;
-    bool bxOn;
+private:
+    // TButtonType eType; //?
+    TSubModel *ModelOn, *ModelOff, *ModelxOn;
+    bool On;
+    bool xOn;
     void Update();
 
-  public:
+public:
     TAirCoupler();
     ~TAirCoupler();
+
     void Clear();
+    void Init(std::string const &asName, TModel3d *Model);
+    void Load(cParser *Parser, TModel3d *Model);
+    //  inline bool Active() { if ((ModelOn)||(ModelOff)) return true; return false;};
+    int GetStatus();
     inline void TurnOn()
     {
-        bOn = true;
-        bxOn = false;
+        On = true;
+        xOn = false;
         Update();
     };
     inline void TurnOff()
     {
-        bOn = false;
-        bxOn = false;
+        On = false;
+        xOn = false;
         Update();
     };
     inline void TurnxOn()
     {
-        bOn = false;
-        bxOn = true;
+        On = false;
+        xOn = true;
         Update();
     };
-    //  inline bool Active() { if ((pModelOn)||(pModelOff)) return true; return false;};
-    int GetStatus();
-    void Init(std::string const &asName, TModel3d *pModel);
-    void Load(cParser *Parser, TModel3d *pModel);
-    //  bool Render();
 };
-
-//---------------------------------------------------------------------------

commit cb4e3bbbf332509e005f34b9d75e7665945f9c9b

fix air coupler regression in 3fe96854

AirCoupler.cpp
@@ -20,14 +20,16 @@ AirCoupler::~AirCoupler()
 }
 
 /**
- * \return 1 when \(straight\) TModel3d \c ModelOn exists
+ * \return 1 when \(straight\) TModel3d \c only ModelOn exists
  * \return 2 when \(slanted\) TModel3d \c ModelxOn exists
  * \return 0 when neither of them exist
  */
 int AirCoupler::GetStatus()
 {
-    if (ModelOn) return 1;
-    if (ModelxOn) return 2;
+    if (ModelxOn)
+		return 2;
+    if (ModelOn)
+		return 1;
     return 0;
 }
 