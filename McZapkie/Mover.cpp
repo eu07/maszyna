d1d5841d60c0de31ae487a2a855fb2d5624323bc
Królik Uszasty <bunny@awril.pl>
2019-10-18 23:30:29 +0200

    Fix for BrakeHandle in DMUs after emergency braking

--- McZapkie/Mover.cpp
+++ McZapkie/Mover.cpp
@@ -3987,7 +3987,7 @@ void TMoverParameters::UpdatePipePressure(double dt)
              || ( ( BrakeHandle != TBrakeHandle::MHZ_EN57 )
                && ( BrakeHandle != TBrakeHandle::MHZ_K8P ) ) ) {
 				double pos = BrakeCtrlPosR;
-				if (SpeedCtrl && SpeedCtrlUnit.BrakeIntervention && !SpeedCtrlUnit.Standby) {
+				if (SpeedCtrlUnit.IsActive && SpeedCtrlUnit.BrakeIntervention && !SpeedCtrlUnit.Standby) {
 					pos = Handle->GetPos(bh_NP);
 					if (SpeedCtrlUnit.BrakeInterventionBraking)
 						pos = Handle->GetPos(bh_FB);
