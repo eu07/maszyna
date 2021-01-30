4e6b2ecedd93e0758bb4841f8a447f46eadc4cf4
milek7 <me@milek7.pl>
2021-01-30 23:43:03 +0100

    bump legacy ogl to 2.1

--- application.cpp
+++ application.cpp
@@ -777,11 +777,12 @@ eu07_application::init_glfw() {
         }
         Global.gfx_shadergamma = false;
         glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE );
-        glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 0 );
 #ifndef EU07_USEIMGUIIMPLOPENGL2
         glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+        glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 0 );
 #else
         glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 2 );
+        glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
 #endif
     }
 
