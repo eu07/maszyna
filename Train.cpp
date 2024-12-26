532c5ecf8ff3ba9efd47a60cabd52b0a370a2ac9
Hirek <polomarket73@gmail.com>
2024-12-26 18:48:40 +0100

    Add drawn and returned power values for pyrthon
    Added power_drawn and power_returned to dict (both double)

--- Train.cpp
+++ Train.cpp
@@ -741,6 +741,9 @@ std::shared_ptr<dictionary_source> TTrain::GetTrainState( dictionary_source cons
     dict->insert( "im", std::abs(  mvControlled->Im ) );
     dict->insert( "fuse", mvControlled->FuseFlag );
     dict->insert( "epfuse", mvOccupied->EpFuse );
+	dict->insert( "power_drawn", mvOccupied->EnergyMeter.first);
+	dict->insert( "power_returned", mvOccupied->EnergyMeter.second);
+
     // induction motor state data
     char const *TXTT[ 10 ] = { "fd", "fdt", "fdb", "pd", "pdt", "pdb", "itothv", "1", "2", "3" };
     char const *TXTC[ 10 ] = { "fr", "frt", "frb", "pr", "prt", "prb", "im", "vm", "ihv", "uhv" };
