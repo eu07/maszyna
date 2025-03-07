7cfb811f7da52c388bdbbb77a577f6d682d205ea
Hirek <polomarket73@gmail.com>
2025-03-07 02:11:27 +0100

    Typo fix

--- drivermouseinput.cpp
+++ drivermouseinput.cpp
@@ -1115,7 +1115,7 @@ drivermouse_input::default_bindings() {
 		{ "invertertoggle12_bt:",{
 			user_command::invertertoggle12,
 			user_command::none } },
-        { "wieprs_sw:",{
+        { "wipers_sw:",{
 			user_command::wiperswitchincrease,
 			user_command::wiperswitchdecrease
          } },
